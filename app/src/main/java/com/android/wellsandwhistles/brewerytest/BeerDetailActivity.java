0c560e31555703ca578dd539e72361d46552cf74
awells111 <awells111@gmail.com>
2017-06-14 13:31:03 -0400

    Initial Commit

---
+++ app/src/main/java/com/android/wellsandwhistles/brewerytest/BeerDetailActivity.java
@@ -0,0 +1,16 @@
+package com.android.wellsandwhistles.brewerytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Owner on 6/14/2017.
+ */
+
+public class BeerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/android/wellsandwhistles/brewerytest/data/Beer.java
@@ -0,0 +1,8 @@
+package com.android.wellsandwhistles.brewerytest.data;
+
+/**
+ * Created by Owner on 6/14/2017.
+ */
+
+public class Beer {
+}
